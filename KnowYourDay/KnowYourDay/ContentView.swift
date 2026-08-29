91cfbc2a912786c653c97afff498efdb284446d0
Sakshi Singh <sakshidevacc@gmail.com>
2026-08-29 15:00:06 +0530

    Add base project

---
+++ KnowYourDay/KnowYourDay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KnowYourDay
+//
+//  Created by Sakshi Singh on 29/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, World!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
